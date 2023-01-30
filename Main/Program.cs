4ef80c7b5b8356e14690c17ec591e99db4944d4e
shahrom123 <118911986+shahrom123@users.noreply.github.com>
2023-01-30 22:38:27 +0500

    first commit

---
+++ Main/Program.cs
@@ -0,0 +1,124 @@
+﻿
+using System.Linq;
+
+// Task 1:
+
+//var array = new[] { 1, 100, 3, 4, 7 }; 
+//var max= array.Max();
+//var min  = array.Min();
+
+
+//for (int i = 0; i < array.Length; i++)
+//{ 
+//    Console.WriteLine(array[i]);
+//}
+
+//...................................................
+// Task 2:
+//var array = new [] { 1, 2, 3, 4, 5 };
+//var sum = array.Sum();
+//Console.Write("Sum = ");
+//Console.WriteLine(sum);
+
+//....................................................
+
+//Task 4:
+//var array = new[] { 1, 3, 3, 4, 2 };
+//var min = array.Order().Distinct().Take(3).ToList();
+//foreach (var item in min)
+//{
+//    Console.WriteLine(item);
+//}
+
+//....................................................
+
+// Task 5:
+//var array = new []{ 5, 4, 3, 2, 1 };
+//var average = array.Average();
+//Console.Write("Average = ");
+//Console.WriteLine(average);
+
+//....................................................
+
+////Task 6:
+//var array = new[] { 1, 2, 3, 4, 2, 23, 23 };
+//var max = array.OrderDescending().Distinct().Take(3).ToList();
+//foreach (var item in max)
+//{
+//    Console.WriteLine(item);
+//}
+
+//....................................................
+
+//Task 7 :
+//var list = new[] { 1, 2, 3, 1, 2 };
+//foreach (var ar in list)
+//{
+//    Console.WriteLine(ar * ar);
+//}
+
+//....................................................
+
+//Task 8 :
+//var list = new[] { 1, 2, 3,  1, 2};
+//foreach (var ar in lists)
+//{
+//    Console.WriteLine(ar*ar*ar);
+//}
+
+//....................................................
+
+// Task 11:
+//var list = new[] { 3, 3, 2, 2, 1, 1, 2};
+//var odd = list.Where(x => x % 2 != 0).ToList();
+
+//foreach (var od in odd)
+//{
+//    Console.WriteLine(od);
+//}
+
+//....................................................
+
+// Task 12:
+//var list = new[] { 13, 2 ,5 ,3 ,11, 7};
+//var odd = list.Where(x => x % 2 != 0).Order();
+
+//foreach (var od in odd)
+//{
+//    Console.WriteLine(od);
+//}
+
+//....................................................
+
+//// Task 13:
+//var list = new[] { 1, 2 ,3 ,4, 5 ,6 ,7, 8 };
+//var odd = list.Where(x => x % 2 != 0).ToList();
+
+//foreach (var od in odd)
+//{
+//    Console.WriteLine(od*od);
+//}
+
+//..................................................
+
+// Task 14:
+//var list = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+//var evens = list.Where(x => x % 2 == 0).ToList();
+
+//foreach (var ev in evens)
+//{
+//    Console.WriteLine(ev *ev);
+//}
+
+//...................................................
+//// Task 15:
+//var list = new[] { 1, 2, 3, 4, 5 };
+//var odd = list.Where(x => x % 2 != 0).ToList();
+
+//foreach (var od in odd)
+//{
+//    Console.WriteLine(od * od * od);
+//}
+
+
+//....................................................
